a5eb0bc9451ba6dd29c5ab3e8d6f005d42fe801a
HuntaBadday <huntabadday@gmail.com>
2025-07-23 13:13:14 -0300

    Creates buffer and RAM base

---
+++ HuntasICLib/src/server/Memory/BufferFIFO.cs
@@ -0,0 +1,5 @@
+namespace HuntasICLib.Memory;
+
+public class BufferFIFO {
+    
+}
---
+++ HuntasICLib/src/server/Memory/BufferLIFO.cs
@@ -0,0 +1,77 @@
+namespace HuntasICLib.Memory;
+
+public class BufferFIFO8b {
+    public bool isFull = false;
+    public bool dataAvailable = false;
+    
+    byte[] memory;
+    
+    int writeVec;
+    int readVec;
+    
+    public BufferFIFO8b(int size) {
+        memory = new byte[size];
+    }
+    
+    public void Reset() {
+        writeVec = 0;
+        readVec = 0;
+        isFull = false;
+        dataAvailable = false;
+    }
+    
+    public void Write(byte data) {
+        if (isFull) return;
+        memory[writeVec++] = data;
+        writeVec %= memory.Length;
+        dataAvailable = true;
+        if (writeVec == readVec) isFull = true;
+    }
+    
+    public byte Read() {
+        if (writeVec == readVec && !isFull) return 0;
+        byte output = memory[readVec++];
+        readVec %= memory.Length;
+        isFull = false;
+        if (writeVec == readVec) dataAvailable = false;
+        return output;
+    }
+}
+
+public class BufferFIFO16b {
+    public bool isFull = false;
+    public bool dataAvailable = false;
+    
+    ushort[] memory;
+    
+    int writeVec;
+    int readVec;
+    
+    public BufferFIFO16b(int size) {
+        memory = new ushort[size];
+    }
+    
+    public void Reset() {
+        writeVec = 0;
+        readVec = 0;
+        isFull = false;
+        dataAvailable = false;
+    }
+    
+    public void Write(ushort data) {
+        if (isFull) return;
+        memory[writeVec++] = data;
+        writeVec %= memory.Length;
+        dataAvailable = true;
+        if (writeVec == readVec) isFull = true;
+    }
+    
+    public ushort Read() {
+        if (writeVec == readVec && !isFull) return 0;
+        ushort output = memory[readVec++];
+        readVec %= memory.Length;
+        isFull = false;
+        if (writeVec == readVec) dataAvailable = false;
+        return output;
+    }
+}
