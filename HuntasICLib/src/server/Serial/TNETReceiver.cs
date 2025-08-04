9db883f71e735bbd5c140e3f37e77966ffb5aba9
HuntaBadday <huntabadday@gmail.com>
2025-08-04 02:21:46 -0300

    Adds TNET receiver and transmitter

---
+++ HuntasICLib/src/server/Serial/TNETReceiver.cs
@@ -0,0 +1,109 @@
+using System;
+namespace HuntasICLib.Serial;
+
+// A class to receive TNET packets
+public class TNETReceiver {
+    private const int MODE_IDLE = 0;
+    private const int MODE_BYTE_RECV = 1;
+    private const int MODE_IPG_WAIT = 2;
+    private const int MODE_FAIL_WAIT = 3;
+    private int currentMode;
+    
+    private int bitCounter;
+    private byte currentByte; // Current byte to receive
+    
+    private byte[] receiveBuffer = new byte[1024]; // Current buffer for receiving
+    private int receiveIndex; // Buffer index
+    
+    private List<byte[]> packetStack = new List<byte[]>(); // All packets received
+    
+    public void Reset(){
+        currentMode = MODE_IPG_WAIT;
+        bitCounter = 0;
+        packetStack.Clear();
+    }
+    
+    // Update logic
+    public void LogicUpdate(bool pinState) {
+        switch (currentMode) {
+            case MODE_IDLE: {
+                if (pinState) {
+                    currentMode = MODE_BYTE_RECV;
+                    receiveIndex = 0;
+                    bitCounter = 0;
+                }
+                break;
+            }
+            case MODE_BYTE_RECV: {
+                currentByte >>= 1;
+                if (pinState) currentByte |= 0x80;
+                if (++bitCounter == 8) {
+                    receiveBuffer[receiveIndex++] = currentByte;
+                    currentMode = MODE_IPG_WAIT;
+                    bitCounter = 0;
+                }
+                break;
+            }
+            case MODE_IPG_WAIT: {
+                if (!pinState) {
+                    if (++bitCounter == 12) {
+                        ProcessReceivedPacket();
+                        currentMode = MODE_IDLE;
+                    }
+                } else if (receiveIndex < 1024) {
+                    currentMode = MODE_BYTE_RECV;
+                    bitCounter = 0;
+                } else {
+                    currentMode = MODE_FAIL_WAIT;
+                    bitCounter = 0;
+                }
+                break;
+            }
+            case MODE_FAIL_WAIT: {
+                if (!pinState) {
+                    if (++bitCounter == 12) {
+                        currentMode = MODE_IDLE;
+                    }
+                } else {
+                    bitCounter = 0;
+                }
+                break;
+            }
+        }
+    }
+    
+    // Get the next packet in the packet queue
+    public byte[] GetNextPacket() {
+        if (packetStack.Count > 0) {
+            byte[] packet = packetStack[0];
+            packetStack.RemoveAt(0);
+            return packet;
+        }
+        return null;
+    }
+    
+    // Check if there is an available packet
+    public bool Available() {
+        return packetStack.Count > 0;
+    }
+    
+    private void ProcessReceivedPacket() {
+        if (receiveIndex < 5) return;
+        
+        uint sum = 0;
+        for (int i = 0; i < receiveIndex-4; i++) {
+            sum += receiveBuffer[i];
+        }
+        
+        uint checksum = 0;
+        checksum |= receiveBuffer[receiveIndex-4];
+        checksum |= (uint)receiveBuffer[receiveIndex-3]<<8;
+        checksum |= (uint)receiveBuffer[receiveIndex-2]<<16;
+        checksum |= (uint)receiveBuffer[receiveIndex-1]<<24;
+        
+        if (sum == checksum) {
+            byte[] output = new byte[receiveIndex-4];
+            Buffer.BlockCopy(receiveBuffer, 0, output, 0, receiveIndex-4);
+        }
+    }
+}
---
+++ HuntasICLib/src/server/Serial/TNETTransmitter.cs
@@ -0,0 +1,91 @@
+using System;
+namespace HuntasICLib.Serial;
+
+// A class to send TNET packets
+public class TNETTransmitter {
+    private const int MODE_IDLE = 0;
+    private const int MODE_SEND_START = 1;
+    private const int MODE_BYTE_SEND= 2;
+    private const int MODE_SEND_IPG = 3;
+    private int currentMode;
+    
+    private int bitCounter;
+    private byte currentByte; // Current byte to send
+    
+    private byte[] sendBuffer; // Current buffer for sending
+    private int sendIndex; // Buffer index
+    
+    private List<byte[]> packetStack = new List<byte[]>(); // All packets to send
+    
+    public void Reset(){
+        currentMode = MODE_SEND_IPG;
+        bitCounter = 0;
+        packetStack.Clear();
+    }
+    
+    // Update logic
+    public bool LogicUpdate() {
+        bool output = false;
+        switch (currentMode) {
+            case MODE_IDLE: {
+                if (packetStack.Count > 0) {
+                    sendBuffer = packetStack[0];
+                    packetStack.RemoveAt(0);
+                    currentMode = MODE_BYTE_SEND;
+                    bitCounter = 0;
+                    sendIndex = 0;
+                    output = true;
+                    
+                    currentByte = sendBuffer[sendIndex++];
+                }
+                break;
+            }
+            case MODE_SEND_START: {
+                output = true;
+                bitCounter = 0;
+                currentByte = sendBuffer[sendIndex++];
+                break;
+            }
+            case MODE_BYTE_SEND: {
+                output = (currentByte & 1) != 0;
+                currentByte >>= 1;
+                if (++bitCounter == 8) {
+                    if (sendIndex == sendBuffer.Length) {
+                        currentMode = MODE_SEND_IPG;
+                        bitCounter = 0;
+                    } else {
+                        currentMode = MODE_SEND_START;
+                    }
+                }
+                break;
+            }
+            case MODE_SEND_IPG: {
+                if (++bitCounter == 12) {
+                    currentMode = MODE_IDLE;
+                }
+                break;
+            }
+        }
+        return output;
+    }
+    
+    // Send a byte array over TNET
+    public void Send(byte[] data) {
+        if (data.Length > 1020 || data.Length == 0) return;
+        
+        uint checksum = 0;
+        for (int i = 0; i < data.Length; i++) {
+            checksum += data[i];
+        }
+        
+        byte[] newData = new byte[data.Length+4];
+        Buffer.BlockCopy(data, 0, newData, 0, data.Length);
+        
+        newData[^4] = (byte)(checksum & 0xff);
+        newData[^3] = (byte)(checksum >> 8 & 0xff);
+        newData[^2] = (byte)(checksum >> 16 & 0xff);
+        newData[^1] = (byte)(checksum >> 24 & 0xff);
+        
+        packetStack.Add(newData);
+    }
+}
