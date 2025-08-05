63204ddb40c8258a24eb8320ec8aff10302d140c
HuntaBadday <huntabadday@gmail.com>
2025-08-05 08:05:57 -0300

    Changes HCPacker, adds HCUnpacker

--- HuntasICLib/src/server/HCPacker.cs
+++ HuntasICLib/src/server/HCPacker.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Text;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace HuntasICLib;
 
 // A class used to pack multiple byte[] into one
-public static class HCPacker {
-    public static void Write(MemoryStream stream, byte[] data) {
-        BinaryWriter w = new BinaryWriter(stream, Encoding.UTF8, true);
+public class HCPacker : IDisposable {
+    MemoryStream memstream;
+    
+    public HCPacker() {
+        memstream = new MemoryStream();
+    }
+    
+    public void Write(byte[] data) {
+        BinaryWriter w = new BinaryWriter(memstream, Encoding.UTF8, true);
         w.Write(data.Length);
         w.Write(data);
     }
     
-    public static byte[] ReadNext(MemoryStream stream) {
-        BinaryReader r = new BinaryReader(stream, Encoding.UTF8, true);
+    public byte[] ToArray() {
+        return memstream.ToArray();
+    }
+    
+    public void Dispose(bool disposing) {
+        if (disposing)
+            memstream.Dispose();
+    }
+
+    public void Dispose() {
+        Dispose(true);
+    }
+}
+
+public class HCUnpacker : IDisposable {
+    MemoryStream memstream;
+    
+    public HCUnpacker(byte[] packedData) {
+        memstream = new MemoryStream(packedData);
+    }
+    
+    public byte[] ReadNext() {
+        BinaryReader r = new BinaryReader(memstream, Encoding.UTF8, true);
         
         try {
             int size = r.ReadInt32();
@@ -20,5 +49,14 @@ public static class HCPacker {
         } catch (EndOfStreamException) {
             return null;
         }
+    }
+    
+    public void Dispose(bool disposing) {
+        if (disposing)
+            memstream.Dispose();
+    }
+
+    public void Dispose() {
+        Dispose(true);
     }
 }
